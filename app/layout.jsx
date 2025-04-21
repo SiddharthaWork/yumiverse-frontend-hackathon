0792b6594878c8394adee16d80344dc02950bbdc
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-04-21 18:20:43 +0545

    Changes in Ui

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,12 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Header from "@/components/Header"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
+
+// adding image in the head
+
 
 export const metadata = {
   title: "Yumiverse - Food Recipe Website",
@@ -12,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" type="image/png" href="/chef.png" />
+      </Head>
       <body className={inter.className}>
       <Header />
         {children}
